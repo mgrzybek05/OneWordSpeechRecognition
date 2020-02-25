9f876dca6877bc6f83a992cecb334dda51f711b8
mjbaucas <mjbaucas@gmail.com>
2020-02-25 22:46:10 +0000

    Reupload new file system

---
+++ classification/classifier.py
@@ -0,0 +1,19 @@
+from keras.models import load_model
+from numpy import array, asarray, shape
+
+from dataset import DatasetGenerator
+
+def classify_sound(self, model, sound_file):
+	LABELS = 'no yes'.split()
+	dsGen = DatasetGenerator(label_set=LABELS) 
+	x = array(self.dsGen.process_wav_file(sound_file))
+	x = asarray(x).reshape(-1, 177, 98, 1)
+	
+	#make a prediction
+	y = model.predict(x)
+	prediction = y[0]
+
+	if prediction[0] > 0.99:
+	    return(1)
+	else:
+	    return(0)
