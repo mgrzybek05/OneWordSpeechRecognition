4448a26e43dbfe633a67b57c90d791396a02df67
mjbaucas <mjbaucas@gmail.com>
2020-02-27 18:58:02 -0500

    Latency results for 3 customers

--- communication/server-classifier.py
+++ communication/server-classifier.py
@@ -4,28 +4,32 @@ import sys
 import time
 from datetime import datetime
 
-sys.path.insert(0, "../classification")
-from classifier import classify_sound
+#sys.path.insert(0, "../classification")
+#from classifier import classify_sound
 
 address_list = [
     '10.11.155.159',
-    '10.11.201.222',
+    '10.11.182.104',
+    '10.11.191.107',
+    '10.11.236.131',
     '10.11.216.196',
-    '10.11.186.106',
-    '10.11.177.188',
-    '10.11.249.2'
+    '10.11.201.222',
+    '10.11.154.138',
+    '10.11.150.163',
+    '10.11.217.179'
+
 ]
 
 if __name__ == "__main__":
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(("10.11.239.102", 32500))
+    server.bind(("10.11.235.35", 32500))
     server.listen(10)
 
     id_counter = 0
-    time_file = open("latency_non2.txt", "a+")
     it_counter = 0
     while True:
+        time_file = open("latency_non3.txt", "a+")
         connection, address = server.accept()
         print(address)
         if address[0] != address_list[id_counter]:
@@ -55,8 +59,8 @@ if __name__ == "__main__":
             if id_counter >= len(address_list):
                 id_counter = 0
             it_counter+=1
-
-            if it_counter >= 60:
+            time_file.close()
+            if it_counter >= 90:
                 break
             print("{} {}".format(it_counter, id_counter))
             
