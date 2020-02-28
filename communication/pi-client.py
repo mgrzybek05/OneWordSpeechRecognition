29d1e58eb486d373cbb0c794902aa4d6325aaea7
mjbaucas <mjbaucas@gmail.com>
2020-02-28 12:34:59 -0500

    Fixed socket logic

--- communication/pi-client.py
+++ communication/pi-client.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
 	limit = 20
 	while exit_client == False:	
 		file_name = "testsound.wav"
-		latency_name = "non_lat_1b.txt"
-		ip_address = "10.11.182.104"
+		latency_name = "non_lat_1a.txt"
+		ip_address = "10.11.155.159"
 	
 		sound_file = open(file_name, "rb")
 		latency_file = open(latency_name, "a+")
@@ -19,10 +19,8 @@ if __name__ == "__main__":
 		done = False
 		while done == False:
 			try:
-				skip = 0
 				time.sleep(1) # Simulate 10 seconds of data collection
 				client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				client.settimeout(4)
 				print("Connecting to server")
 				start = datetime.now().timestamp()
 				# Fog
@@ -41,15 +39,10 @@ if __name__ == "__main__":
 						print(e)
 						time.sleep(2)
 						break
-				try:
-					message = client.recv(1024)
-					if message == b'Done':
-						end = datetime.now().timestamp()
-						latency_file.write("Start {} {}\n".format(ip_address, start))
-						latency_file.write("End {} {}\n".format(ip_address, end))
-						done = True
-				except socket.timeout:
-					done = False	
+				end = datetime.now().timestamp()
+				latency_file.write("Start {} {}\n".format(ip_address, start))
+				latency_file.write("End {} {}\n".format(ip_address, end))
+				done = True
 				time.sleep(2)
 			except Exception as e:
 				print(e)
