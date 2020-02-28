95386180d581c09e04d94a57fce2f2660181198e
mjbaucas <mjbaucas@gmail.com>
2020-02-28 11:01:37 -0500

    Changed where latency is being measured

--- communication/pi-client.py
+++ communication/pi-client.py
@@ -1,41 +1,60 @@
 import os
 import time
 import socket
+from datetime import datetime
 
 if __name__ == "__main__":	
 	exit_client = False
+	it_counter = 0
+	limit = 20
 	while exit_client == False:	
 		file_name = "testsound.wav"
-
+		latency_name = "non_lat_1a.txt"
+		ip_address = "10.11.155.159"
+	
 		sound_file = open(file_name, "rb")
+		latency_file = open(latency_name, "a+")
 		sound_data = sound_file.read()
 		
-		connected = False
-		time.sleep(10) # Simulate 10 seconds of data collection
-		while connected == False:
+		done = False
+		while done == False:
 			try:
-				print("Connecting to server")
+				time.sleep(1) # Simulate 10 seconds of data collection
 				client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				print("Connecting to server")
+				start = datetime.now().timestamp()
 				# Fog
 				#client.connect(("10.16.9.113", 32500))
 				# Server
-				client.connect(("10.11.235.35", 32500))
-				connected = True
+				client.connect(("10.11.232.95", 32500))
+				latency_file.write("Start {} {}\n".format(ip_address, start))
+				
+				while sound_data:
+					data = sound_data[:2048]
+					if not data:
+						break
+					try:
+						sent = client.send(data)
+						sound_data = sound_data[sent:]
+					except Exception as e:
+						time.sleep(2)  
+						break
+
+				end = datetime.now().timestamp()
+				latency_file.write("End {} {}\n".format(ip_address, end))
+				time.sleep(2)
+				done = True
 			except Exception as e:
+				print(e)
 				time.sleep(2)
 				client.close()
-				connected = False
+				done = False
 		
-		while sound_data:
-			data = sound_data[:2048]
-			if not data:
-				break
-			try:
-				sent = client.send(data)
-				sound_data = sound_data[sent:]
-			except Exception as e:
-				time.sleep(2)  
-				break
-		time.sleep(2)
+		it_counter+=1
+		print(it_counter)
+		if it_counter >= limit:
+			exit_client = True
+				
+
 
 	
