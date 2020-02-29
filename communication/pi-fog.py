0b58d453b7a62fd8ad72b244d7befa3a9de86067
mjbaucas <mjbaucas@gmail.com>
2020-02-28 20:34:07 -0500

    results 1a

--- communication/pi-fog.py
+++ communication/pi-fog.py
@@ -6,22 +6,29 @@ from datetime import datetime
 
 def send_to_server(results):
 	connected = False
+	latency_name = "fog_lat_1a.txt"
+	ip_address = "10.11.155.159"
+	
+	latency_file = open(latency_name, "a+")
 	while connected == False:
 		try:
+			start = datetime.now().timestamp()
 			print("Connecting to server")
 			client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			client.connect(("10.11.239.102", 32500))
+			client.connect(("10.11.232.95", 32500))
 			sent = client.send(str(results).encode())
-			time.sleep(1)	
 			from_server = client.recv(1024)
 			if from_server == b'Done':
-				connected = True				
+				connected = True
+				end = datetime.now().timestamp()
+				latency_file.write("Start {} {}\n".format(ip_address, start))
+				latency_file.write("End {} {}\n".format(ip_address, end))				
 		except Exception as e:
 			time.sleep(1)
 			connected = False
 
 if __name__ == "__main__":
-	for i in range(10):
+	for i in range(60):
 		value = 1
 		send_to_server(value)
-	
+					
